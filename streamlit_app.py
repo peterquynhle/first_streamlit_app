30b3426a390c6f1ced91f12f61408a5eb76cd674
Peter (Quynh) Le <32576700+peterquynhle@users.noreply.github.com>
2023-02-23 16:24:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,12 @@ streamlit.dataframe(fruits_to_show)
 # Lesson 9
 #Let's Call the Fruityvice API from Our Streamlit App!
 
+# New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
