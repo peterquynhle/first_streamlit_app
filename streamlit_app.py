bd52c73ebdee2a95afaf45f1b50ca104d0acb5df
Peter (Quynh) Le <32576700+peterquynhle@users.noreply.github.com>
2023-08-15 15:16:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,19 +38,16 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+ fruit_choice)
-# comment out this line to remove json data from the app: "streamlit.text(fruityvice_response.json())"
-
-#🥋 Making the JSON Look Good
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#Output it in the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  esle:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+ fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.header("Fruityvice Fruit Advice!")
 
@@ -65,7 +62,14 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
-insert into fruit_load_list values ('from streamlit');
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row)
+
+insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlit');
 select * from fruit_load_list;
 
 streamlit.write('thanks for adding', add_my_fruit)
