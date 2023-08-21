6ba4a7f41a9d46a70a294cca46dd1d8548dfbc27
Peter (Quynh) Le <32576700+peterquynhle@users.noreply.github.com>
2023-08-21 10:04:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast')
+streamlit.text('Omega 3 & Blueberry Oatmel')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
